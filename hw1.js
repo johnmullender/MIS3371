f9e2825e33ea42004d2b55813bbc3bbc85840c0e
johnmullender <johnmullender2x@gmail.com>
2026-02-26 04:04:38 -0600

    Initial Commit

---
+++ hw1.js
@@ -0,0 +1,21 @@
+/*
+    Program Name: index.html
+    Author: John Mullender
+    Date created: February 22, 2026
+    Date modified: February 26, 2026
+    Version: 1.0
+    Purpose: Homework1 JavaScript Form
+*/
+
+// dynamic date js code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementByID("today").innerHTML = text;
+
+// range slide js code
+let slider = document.getElementById("range")
+    let output = document.getElementById("range-slider")
+    output.innerHTML = slider.value;
+
+    slider.oninput = function () {output.innerHTML - this.value;};
+
